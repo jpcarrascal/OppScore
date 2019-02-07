8feaee894bbfb5aa5f896c188110bfe0c4c781b2
JP <v-jpcarr@microsoft.com>
2019-02-07 11:14:29 +0100

    - Results sorted by OppScore (descendent) - OppScores rounded to 2 decimal digits - Plot adjusted to start from 0

--- OppScore.R
+++ OppScore.R
@@ -42,11 +42,11 @@ plotOppScore <- function(values)
 {
   grey <- "#AAAAAA"
   par(xpd=F)
-  plot(NULL, xlim=c(1,10), ylim=c(1,10), yaxt="n", xaxt="n", xaxs="i", yaxs="i", xlab = "Importance", ylab = "Satisfaction", main="Opportunity Landscape")
+  plot(NULL, xlim=c(0,10), ylim=c(0,10), yaxt="n", xaxt="n", xaxs="i", yaxs="i", xlab = "Importance", ylab = "Satisfaction", main="Opportunity Landscape")
   axis(1, at = c(1:10), labels=c(1:10), cex=1)
   axis(2, at = c(1:10), labels=c(1:10), cex=1)
-  segments(1, 1, 10, 10, lty = 1, col=grey)
-  segments(5.5, 1, 10, 10, lty = 1, col=grey)
+  segments(0, 0, 10, 10, lty = 1, col=grey)
+  segments(5, 0, 10, 10, lty = 1, col=grey)
   abline(a = -11.5, b = (10-1)/(10-5.5), lty = 3, col=grey)
   abline(a = -14.5, b = (10-1)/(10-5.5), lty = 3, col=grey)
   text(3,5.8,"Overserved",col=grey)
@@ -56,9 +56,19 @@ plotOppScore <- function(values)
   text(9,4.5,"Opp>12",col=grey)
   text(9.5,3,"Opp>15",col=grey)
   par(xpd=T)
-  points(values$importance, values$satisfaction, xlim=c(1,10), ylim=c(1,10), col=2, pch=19, cex=2)
+  points(values$importance, values$satisfaction, xlim=c(0,10), ylim=c(0,10), col=2, pch=19, cex=2)
   with(values, text(satisfaction~importance, labels = c(1:length(values$outcome)), pos = 2, col=1, srt=0, cex=0.8))
-  legend(1.1,9.9, paste(c(1:length(values$outcome)),values$outcome,sep=". "), cex=0.85, x.intersp=0.0)
+  legend(0.1,9.9, paste(c(1:length(values$outcome)),values$outcome,sep=". "), cex=0.85, x.intersp=0.0)
+}
+
+# Borrowed from http://jeromyanglim.tumblr.com/post/50228877196/round-numbers-in-data-frame-that-contains-non
+round_df <- function(x, digits) {
+  # round all numeric variables
+  # x: data frame 
+  # digits: number of digits to round
+  numeric_columns <- sapply(x, class) == 'numeric'
+  x[numeric_columns] <-  round(x[numeric_columns], digits)
+  x
 }
 
 # Read the data and run the functions:
@@ -70,7 +80,8 @@ imp <- aggregate(importance~outcome, inputData, count_positives, threshold=thres
 sat <- aggregate(satisfaction~outcome, inputData, count_positives, threshold=threshold)
 values <- merge(x = imp, y = sat, by = "outcome", all = TRUE)
 values$oppscore = op_score(values$importance, values$satisfaction)
-print(values)
+values <- values[order(-values$oppscore),]
+print(round_df(values, 2))
 plotOppScore(values)
 
 
