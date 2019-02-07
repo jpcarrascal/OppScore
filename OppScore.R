e893596e55a35379e37720d1323af0399ada76b2
JP <v-jpcarr@microsoft.com>
2019-02-07 11:27:26 +0100

    - Axes adjust automatically to 0-10 if importance or satisfaction are lower than 1. - Legend background is transparent, just in case.

--- OppScore.R
+++ OppScore.R
@@ -42,9 +42,14 @@ plotOppScore <- function(values)
 {
   grey <- "#AAAAAA"
   par(xpd=F)
-  plot(NULL, xlim=c(0,10), ylim=c(0,10), yaxt="n", xaxt="n", xaxs="i", yaxs="i", xlab = "Importance", ylab = "Satisfaction", main="Opportunity Landscape")
-  axis(1, at = c(1:10), labels=c(1:10), cex=1)
-  axis(2, at = c(1:10), labels=c(1:10), cex=1)
+  if( (sum(values$importance<1) > 0) || (sum(values$satisfaction<1) > 0) )
+    lowerLimit <- 0
+  else
+    lowerLimit <- 1
+  print(lowerLimit)
+  plot(NULL, xlim=c(lowerLimit,10), ylim=c(lowerLimit,10), yaxt="n", xaxt="n", xaxs="i", yaxs="i", xlab = "Importance", ylab = "Satisfaction", main="Opportunity Landscape")
+  axis(1, at = c(lowerLimit:10), labels=c(lowerLimit:10), cex=1)
+  axis(2, at = c(lowerLimit:10), labels=c(lowerLimit:10), cex=1)
   segments(0, 0, 10, 10, lty = 1, col=grey)
   segments(5, 0, 10, 10, lty = 1, col=grey)
   abline(a = -11.5, b = (10-1)/(10-5.5), lty = 3, col=grey)
@@ -56,9 +61,9 @@ plotOppScore <- function(values)
   text(9,4.5,"Opp>12",col=grey)
   text(9.5,3,"Opp>15",col=grey)
   par(xpd=T)
-  points(values$importance, values$satisfaction, xlim=c(0,10), ylim=c(0,10), col=2, pch=19, cex=2)
+  points(values$importance, values$satisfaction, xlim=c(lowerLimit,10), ylim=c(lowerLimit,10), col=2, pch=19, cex=2)
   with(values, text(satisfaction~importance, labels = c(1:length(values$outcome)), pos = 2, col=1, srt=0, cex=0.8))
-  legend(0.1,9.9, paste(c(1:length(values$outcome)),values$outcome,sep=". "), cex=0.85, x.intersp=0.0)
+  legend(bg="transparent",lowerLimit+0.1,9.9, paste(c(1:length(values$outcome)),values$outcome,sep=". "), cex=0.85, x.intersp=0.0)
 }
 
 # Borrowed from http://jeromyanglim.tumblr.com/post/50228877196/round-numbers-in-data-frame-that-contains-non
